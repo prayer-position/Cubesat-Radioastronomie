ce62c441a0c3ff4ac357a104c0f498b024e4d34d
Charles Didier <charlesdidiermoi@gmail.com>
2026-02-05 09:23:04 +0100

    Solved path issue & made test_gen.py more flexible

--- src/test_gen.py
+++ src/test_gen.py
@@ -2,7 +2,7 @@ import numpy as np
 from src.modulation import gmsk_mod
 from src.channel import awgn
 
-def generate_test_signal(snr, L, fc):
+def generate_test_signal(n_bits, snr, L, fc):
     """
     Generates a single test signal burst for evaluation.
     
@@ -12,9 +12,8 @@ def generate_test_signal(snr, L, fc):
     Returns:
         (bits, s_noisy, s_clean): Tuple of noisy and clean signal arrays.
     """
-    bits_per_sample = 256
     # 1. Generate random bits
-    a = np.random.randint(2, size=bits_per_sample)
+    a = np.random.randint(2, size=n_bits)
     
     # 2. Modulate (Clean Signal)
     _, s_clean = gmsk_mod(a, fc, L, BT=0.3)
